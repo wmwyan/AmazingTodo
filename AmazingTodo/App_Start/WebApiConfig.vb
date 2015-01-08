03f2da890cfba3836b1ca012c7bf9e36a2785a71
wmwyan <wmwyan@hotmail.com>
2015-01-08 16:08:24 +0800

    update

---
+++ AmazingTodo/App_Start/WebApiConfig.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web.Http
+
+Public Class WebApiConfig
+    Public Shared Sub Register(ByVal config As HttpConfiguration)
+        config.Routes.MapHttpRoute( _
+            name:="DefaultApi", _
+            routeTemplate:="api/{controller}/{id}", _
+            defaults:=New With {.id = RouteParameter.Optional} _
+        )
+        
+        '取消注释下面的代码行可对具有 IQueryable 或 IQueryable(Of T) 返回类型的操作启用查询支持。
+        '若要避免处理意外查询或恶意查询，请使用 QueryableAttribute 上的验证设置来验证传入查询。
+        '有关详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=279712。
+        'config.EnableQuerySupport()
+        
+        '若要在应用程序中禁用跟踪，请注释掉或删除以下代码行
+        '有关详细信息，请参阅: http://www.asp.net/web-api
+        config.EnableSystemDiagnosticsTracing()
+    End Sub
+End Class
---
+++ AmazingTodo/Areas/HelpPage/Views/Help/DisplayTemplates/ImageSample.vbhtml
@@ -0,0 +1,4 @@
+@Imports AmazingTodo.Areas.HelpPage
+@ModelType ImageSample
+
+<img src="@Model.Src" />
